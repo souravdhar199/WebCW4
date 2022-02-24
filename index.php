96e85a36bd0a3ad8218c04453441efaa31a31032
sourav199 <souravdhar1999@gmail.com>
2022-02-24 16:06:00 -0500

    Done with activity 2

--- index.php
+++ index.php
@@ -6,7 +6,35 @@ function f1() {
   echo "Hello world!";
 }
 f1();
+echo "<br>";
 echo "Exercise 2: Loops";
 echo "<br>";
 echo "<br>";
+for ($x = 0; $x <5; $x++) {
+    for ($j =0 ; $j <= $x; $j++) {
+        echo "*";  
+    }
+    echo "<br>";
+}
+echo "<br>";
+echo "Exercise 3: Fucntion";
+echo "<br>";
+echo "<br>";
+
+function f2($parameter) {
+    for ($i = 0; $i <$parameter; $i++) {
+        for ($j =0 ; $j <= $i; $j++) {
+            echo "*";  
+        }
+        echo "<br>";
+    }
+}
+f2(6);
+
+
+
+
+
+
+
 ?>
